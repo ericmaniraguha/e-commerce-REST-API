cbcd12d5bcb65f7e1f8754cf9294ebbdc928232f
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-07-03 21:54:59 +0200

    Done with all crud operations with carts, orders, products and calculate income

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 const PORT = process.env.PORT;
 
 mongoose
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(PORT || 5000, () => {
   console.log(`The backend server is running on : ....${PORT}`);
