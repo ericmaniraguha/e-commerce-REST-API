be1a980ed7d503deebad13b461655baee275aa24
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-07-03 07:34:51 +0200

    done with create user, login and use token to update username

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,31 @@
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+} = require('./middleware/verifyToken');
+
 const router = require('express').Router();
 
+// UPDATE
+
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
